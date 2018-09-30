2398cb1dd568d023648995fd505c32c707c8b287
/Saurav_Chandra <sauravchandra94@gmail.com>
2018-09-30 09:34:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+	echo "hey Saurav";
+	if(3>0) echo "<br> Chandra";
+
+	if(condition)
+	echo "<script>window.link.href="+$url+"</script>"; //for php redirrect
+
+//   sanskrit/hindi   to english code{keyword} type & vice versa! JS compiler.
+?>
+
+
+
+
+
+
+
+
+
