4c76df35bc564a4f7b26e68b192ca03accc0c10a
/Saurav_Chandra <sauravchandra94@gmail.com>
2018-09-30 10:46:57 +0530

    Update Utopia.js

--- Utopia.js
+++ Utopia.js
@@ -1,1 +1,10 @@
-
+///////////////Made by SauravC.////////////////
+//#########sauravchandra757@yahoo.com#########//
+/////General Object tools//////////////////////
+function OBJS(name,n){  // name=man  or man() , n=number of objects;   Since we can't create arrays of objects directly in JavaScript.
+			var x=new Array(n);
+			for (var i=0;i<x.length;++i) eval("x[i] = new "+name+";");
+			return x;
+} 
+// eg. var x = OBJS('man()',8); // returns man[8] fxn made objects, for class simply type 'man'
+//     Helps in instanciating the whole array at once & returning Array Object.
